37032388b33f916f6dc6a797b01319d4b4aa9afc
bitflipper19 <samyakpandey2007@gmail.com>
2025-12-25 17:18:36 +0530

    initial Commit

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define ip for(int i=0; i<n; ++i) cin>> v[i]
+#define f(i, start, end) for(int i=start; i<end; ++i)
+
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void solve() {
+    cout>> "Hello! World."<< endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ math/add.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define ip for(int i=0; i<n; ++i) cin>> v[i]
+#define f(i, start, end) for(int i=start; i<end; ++i)
+
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void solve() {
+    int a, b;
+    cin>> a>> b;
+    cout<< a-b<< endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
